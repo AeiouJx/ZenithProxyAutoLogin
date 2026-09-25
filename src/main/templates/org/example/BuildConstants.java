bc656b425dc5cc3e057eef5fa7981846e8202fa8
AeiouJx <aeioujx@gmail.com>
2026-09-25 23:16:58 +0800

    Initialize 26.2.0

---
+++ src/main/templates/org/example/BuildConstants.java
@@ -0,0 +1,13 @@
+package ${maven_group};
+
+// The constants are replaced before compilation
+public class BuildConstants {
+
+    public static final String VERSION = "${version}";
+
+    public static final String MC_VERSION = "${mc_version}";
+
+    public static final String PLUGIN_ID = "${plugin_id}";
+
+    public static final String MAVEN_GROUP = "${maven_group}";
+}
